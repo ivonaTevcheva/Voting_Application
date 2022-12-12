ea2f40d51d0c9abe1f9d64b0864224de68dfc473
User <ivonatevceva23@gmail.com>
2022-12-12 01:59:54 +0100

    login

--- app/src/main/java/com/example/voting_application/activities/LoginActivity.java
+++ app/src/main/java/com/example/voting_application/activities/LoginActivity.java
@@ -1,18 +1,28 @@
 package com.example.voting_application.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.voting_application.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
     private Button button, login_btn;
+    private EditText userPassword, userEmail;
     private FirebaseAuth mAuth;
 
     @Override
@@ -20,15 +30,10 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-//        findViewById(R.id.linearLayout2).setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                startActivity(new Intent(LoginActivity.this, SignUpActivity.class));
-//            }
-//        });
-
         button = (Button) findViewById(R.id.signup_btn);
         login_btn = (Button) findViewById(R.id.login_btn);
+        userEmail = findViewById(R.id.user_email);
+        userPassword = findViewById(R.id.user_password);
         mAuth = FirebaseAuth.getInstance();
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,12 +45,42 @@ public class LoginActivity extends AppCompatActivity {
         login_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //mAuth.signInWithCredential();
+                String email = userEmail.getText().toString().trim();
+                String password = userPassword.getText().toString().trim();
+
+                if (!TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches() && !TextUtils.isEmpty(password)) {
+
+                    loginUser(email, password);
+
+                } else {
+                    Toast.makeText(LoginActivity.this, "Please enter your credentials", Toast.LENGTH_SHORT).show();
+                }
             }
         });
-        }
+    }
 
-    public void openRegistration(){
+    private void loginUser(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    FirebaseUser firebaseUser = mAuth.getCurrentUser();
+                    if (firebaseUser.getDisplayName().equals("user")) {
+                        //GOTO USER HOMEPAGE INTENT
+                        Toast.makeText(LoginActivity.this, "User signed in as normal user", Toast.LENGTH_SHORT).show();
+
+                    } else {
+                        //GOTO ADMINISTRATOR HOMEPAGE INTENT
+                        Toast.makeText(LoginActivity.this, "User signed in as administrator", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(LoginActivity.this, "User login failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    public void openRegistration() {
         Intent intent = new Intent(this, SignUpActivity.class);
         startActivity(intent);
     }
--- app/src/main/java/com/example/voting_application/activities/SignUpActivity.java
+++ app/src/main/java/com/example/voting_application/activities/SignUpActivity.java
@@ -18,11 +18,13 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 
 public class SignUpActivity extends AppCompatActivity {
 
     private Button button, signUpBtn;
-    private EditText userName,userEmail, userPassword, userConfirmPassword;
+    private EditText userName, userEmail, userPassword, userConfirmPassword;
     private FirebaseAuth mAuth;
 
     @Override
@@ -78,14 +80,23 @@ public class SignUpActivity extends AppCompatActivity {
         mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
-                if(task.isSuccessful()){
-                    Toast.makeText(SignUpActivity.this, "User created", Toast.LENGTH_SHORT).show();
-
-                }else{
+                if (task.isSuccessful()) {
+                    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                    UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                            .setDisplayName("user")
+                            .build();
+                    user.updateProfile(profileUpdates)
+                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(SignUpActivity.this, "User created", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                } else {
                     Toast.makeText(SignUpActivity.this, "Fail! Try again", Toast.LENGTH_SHORT).show();
-
                 }
-
             }
         }).addOnFailureListener(new OnFailureListener() {
             @Override
@@ -95,8 +106,8 @@ public class SignUpActivity extends AppCompatActivity {
         });
     }
 
-    public void openLogin(){
-        Intent intent = new Intent (this, LoginActivity.class);
+    public void openLogin() {
+        Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
     }
 }
